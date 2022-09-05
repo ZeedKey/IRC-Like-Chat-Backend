79e28aa455d692034a047296e3327b84cd6ba857
Zeedkey <the.furriest.man.ever@gmail.com>
2022-09-05 13:20:48 +0400

    :hammer: Users module refactored

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { UsersResolver } from './users.resolver';
